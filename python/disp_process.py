a978afe06b86880f8c841184aa95543de1420497
andrewb <andrewb@ubuntuserver.(none)>
2013-03-03 14:06:01 +1100

    changes

--- python/disp_process.py
+++ python/disp_process.py
@@ -1,39 +1,17 @@
 import psutil
-import curses 
-from time import sleep
 
-print dir(psutil)
-print dir(curses)
+ignore_keys = ['count','index', 'fromhex', 'hex', 'as_integer_ratio', 'conjugate', 'is_integer']
 
+def get_values(values):
 
-def setup():
-	return curses.initscr()
-
-def p(x, y):
-	return curses.newpad(x,y)
-
-def teardown():
-	curses.nocbreak()
-	stdscr.keypad(0)
-	curses.echo()
-
-def end():
-	curses.endwin()
+	keys = [d for d in dir(values) if not d[0] == '_' and not d in ignore_keys]
+	d = {k:getattr(values, k) for k in keys}
+	print d
 
 if __name__ == "__main__":
-	stdscr = setup()
-	pad = p(100, 100)
-	#  These loops fill the pad with letters; this is
-	# explained in the next section
-	for y in range(0, 100):
-		for x in range(0, 100):
-			try: pad.addch(y,x, ord('a') + (x*x+y*y) % 26 )
-			except curses.error: pass
+	get_values(psutil.virtual_memory())
+	get_values(psutil.cpu_times())
+	get_values(psutil.cpu_percent())
 
-	#  Displays a section of the pad in the middle of the screen
-	pad.refresh( 0,0, 5,5, 20,75)
-
-	#curses.nocbreak(); stdscr.keypad(0); curses.echo()
-	sleep(10)
-	teardown()
-	end()
+	get_values(psutil.Process(307))	
+	
