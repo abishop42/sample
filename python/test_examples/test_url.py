8b7784e473564b33e0ce4e53795c060a55957844
unknown <thekeg@thekeg-PC.(none)>
2013-11-27 18:23:21 +1100

    classy stuff

--- python/test_examples/test_url.py
+++ python/test_examples/test_url.py
@@ -1,47 +1,57 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
-visited_urls = []
 
-results = []
 
-def check_site(urls):
-	#TODO: make sure that base url is tht base of the current url being looked at
-	#      as just setting base url could end up with some bad stuff happening
-	results = []
-	new_urls = []
-	for url in urls:
+class CrawlSite:
+
+	def __init__(self, url, ingore_elements=[]):
+		self.visited_urls = []
+		self.to_visit = [url]
+		self.results = []
+		self.ingore_elements = ingore_elements
+
+	def start(self, break_after=10):
+
+		for url in self.to_visit:
+			self.check_site(url)
+
+		if break_after > 0 and len(self.visited_urls) > break_after:
+			break
+
+	def check_site(self, url):
 		r = requests.get(url)
 		ok = r.status_code == requests.codes.ok
-		results.append({'url': url, 'ok':ok, 'reason':r.reason, 'conent-type':r.headers['content-type']})
-		print (results[-1])
-		visited_urls.append(url)
+		self.results.append({'url': url, 'ok':ok, 'reason':r.reason, 'conent-type':r.headers['content-type']})
+		print (self.results[-1])
+		
+		self.visited_urls.append(url)
+
 		if ok == True:
-			soup = BeautifulSoup(r.text)
-			for link in soup.find_all('a'):
-				if link.has_attr('href'):
-					u = link.get('href')	
-					if u.find('tel:') < 0:
-						if u == '#':
-							u = url + u
-	
-						elif not u[0:4] == 'http':
-							u = url + u
+			self.parse_page(r.text)
 
-						if not u in visited_urls and not u in new_urls and not u == None:
-							new_urls.append(u)
-	
-	for r in results:
-		print (r)
+	def parse_page(self, text):
+		soup = BeautifulSoup(text)
+		for link in soup.find_all('a'):
+			
+			if link.has_attr('href'):
+				u = link.get('href')	
 
-	if new_urls:
-		check_site(new_urls)
+			if u.find('tel:') < 0:
+				if u == '#':
+					u = url + u
+	
+				elif not u[0:4] == 'http':
+					u = url + u
+
+				if not u in self.visited_urls and not u in self.to_visit and not u == None:
+					self.to_visit(u)
+	
 
 
 if __name__ == '__main__':
-	test_urls = ['http://www.messagemedia.com.au']
-	
-	print (check_site(test_urls))
+	crawl = CrawlSite('http://www.messagemedia.com.au'. ['tel:', 'mailto:'])
+	crawl.start()
 
 	
 
